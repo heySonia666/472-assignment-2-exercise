88d721cdc781e0243dcdd6f1b5ec7227575f11c1
heySonia666 <M.Mo2@lse.ac.uk>
2023-11-02 02:45:06 +0000

    update to main

---
+++ transformed initial.R
@@ -0,0 +1,20 @@
+# read and load the dataset "mtcars"
+library(dplyr)
+data("mtcars")
+transform_data <- function(data) {
+  # Check if the 'disp' column exists in the dataset
+  if (!'disp' %in% colnames(data)) {
+    stop("The dataset must contain an 'disp' column for Displacement.")
+  }
+  
+ # Use mutate to apply the transformation
+  data <- data %>%
+    mutate(disp = disp * 2)
+  
+  # Return the transformed dataset
+  return(data)
+}
+
+# Example usage with the mtcars dataset
+transformed_data <- transform_data(mtcars)
+print(transformed_data)
